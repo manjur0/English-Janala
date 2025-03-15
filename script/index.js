f545a58dbac024fb1233b2b457a70b1053129f6a
manjur0 <sfmanjur21@gmail.com>
2025-03-15 17:26:34 +0600

    Add dynamic loading of vocabulary levels and fix section ID

--- script/index.js
+++ script/index.js
@@ -1,1 +1,29 @@
-console.log("Hello World")
+//Get ⚡ All Levels
+//https://openapi.programming-hero.com/api/levels/all
+
+const loadAllLevels = () => {
+  fetch("https://openapi.programming-hero.com/api/levels/all")
+    .then((res) => res.json())
+    .then((data) => displayAllLevels(data.data));
+};
+
+const displayAllLevels = (levels) => {
+  const vocabularies = document.getElementById("vocabularies");
+  vocabularies.textContent = "";
+  console.log(levels);
+  levels.forEach((level) => {
+    const button = document.createElement("button");
+    button.innerHTML = `
+        <button
+              class="btn btn-outline border border-blue-700 text-blue-700 hover:bg-blue-700 hover:text-white"
+            >
+              <img src="assets/fa-book-open.png" alt="" /> Lesson -1
+            </button>
+      
+        `;
+    vocabularies.appendChild(button);
+    console.log(level.id);
+  });
+};
+
+loadAllLevels();
